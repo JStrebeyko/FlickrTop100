50dc86e70f21bee0dd35b0ddd30e4047d383c3e0
Jakub Strebeyko <strebeyko@gmx.com>
2018-10-13 13:35:50 +0200

    main components scaffolding

---
+++ src/components/TopBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="topbar-wrapper">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topbar',
+
+}
+</script>
+
+<style scoped>
+.topbar-wrapper {
+  display: block;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 12rem;
+}
+</style>
---
+++ src/views/MainScreen.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="all">
+    <top-bar/>
+  </div>
+</template>
+
+<script>
+import TopBar from
+export default {
+  name: 'MainScreen'
+}
+</script>
+
+<style>
+
+</style>
