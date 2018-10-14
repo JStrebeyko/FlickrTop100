25048df71da1f713b50958eecd24f5f49e74e0d9
Jakub Strebeyko <strebeyko@gmx.com>
2018-10-14 17:09:58 +0200

    do some input work

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,22 +1,50 @@
 <template>
   <div class="topbar-wrapper">
-
+    <p>{{changed}}</p>
+    <input
+            @input="handleInput"
+            @change="handleChange"
+            v-model="innerValue"/>
   </div>
 </template>
 
 <script>
 export default {
   name: 'topbar',
-
+  props: {
+    value: {
+      type: String,
+    }
+  },
+  data() {
+    return {
+      innerValue: this.value,
+      changed: null
+    }
+  },
+  methods: {
+    handleInput() {
+      // this.$emit('input', this.innerValue)
+    },
+    handleChange() {
+      this.$emit('change', this.innerValue)
+      this.changed=this.innerValue
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .topbar-wrapper {
   display: block;
   position: fixed;
   top: 0;
   width: 100%;
-  height: 12rem;
+  height: 15rem;
+  background: red;
+  p {
+    margin: 1rem;
+    font-size: 10rem;
+  }
 }
 </style>
